d7b7bcc60356717a4b9c9ebf05626675dea18200
Yashraj Jain <yashrajjain726@gmail.com>
2020-12-06 15:51:00 +0530

    OnBoarding Page UI Updated

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:login_ui/coolors.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class LoginPage extends StatelessWidget {
--- lib/onboarding/onboarding.dart
+++ lib/onboarding/onboarding.dart
@@ -1,41 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:login_ui/login/login_page.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class OnBoarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              color: Colors.pink[300],
-            ),
-            Container(
-              width: context.screenWidth,
-              height: context.percentHeight * 80,
-              decoration: BoxDecoration(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.pink[300],
+          ),
+          Container(
+            width: context.screenWidth,
+            height: context.percentHeight * 80,
+            decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(60.0),
-                  bottomRight: Radius.circular(60.0),
-                ),
-              ),
+                  bottomLeft: Radius.circular(60),
+                  bottomRight: Radius.circular(60),
+                )),
+            child: VStack([
+              SvgPicture.asset(
+                "assets/cleaning.svg",
+                fit: BoxFit.cover,
+              ).pOnly(top: 100, bottom: 32),
+              "Cleaning on Demand".text.center.extraBlack.xl3.makeCentered(),
+              8.heightBox,
+              "Book an appointment in less than 60 seconds and get on the schedule as early as tomorrow."
+                  .text
+                  .gray500
+                  .medium
+                  .center
+                  .makeCentered()
+                  .p16()
+            ]),
+          ),
+          Positioned(
+            child: Container(
+              height: context.percentHeight * 15,
               child: VStack(
                 [
-                  Padding(
-                    padding: const EdgeInsets.all(32.0),
-                    child: SvgPicture.asset(
-                      "assets/cleaning.svg",
-                    ),
-                  ),
-                  SizedBox(height: 28),
+                  HStack(
+                    [
+                      VxBox().square(5).gray500.roundedFull.make(),
+                      10.widthBox,
+                      VxBox().square(8).white.roundedFull.make(),
+                      10.widthBox,
+                      VxBox().square(5).gray500.roundedFull.make(),
+                    ],
+                    alignment: MainAxisAlignment.center,
+                  ).wFull(context),
+                  16.heightBox,
+                  Spacer(),
+                  ButtonBar(
+                    mainAxisSize: MainAxisSize.max,
+                    alignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FlatButton(
+                        onPressed: () {},
+                        child: "Skip".text.white.make(),
+                      ),
+                      [
+                        FlatButton(
+                          onPressed: () {},
+                          child: "Next".text.white.make(),
+                        ).px2(),
+                        SvgPicture.asset(
+                          "assets/next.svg",
+                        )
+                      ].hStack()
+                    ],
+                  )
                 ],
-              ),
+                alignment: MainAxisAlignment.start,
+              ).wFull(context),
             ),
-          ],
-        ),
+            bottom: 24,
+          ),
+        ],
       ),
     );
   }
